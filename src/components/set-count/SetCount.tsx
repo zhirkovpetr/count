6d5eb91ec7f918d45c21cedde44cb4a2caa2b108
Petr <zhirkovpetr95@gmail.com>
2023-09-20 11:01:34 +0300

    feat: add SetCount component structures

---
+++ src/components/set-count/SetCount.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const SetCount: React.FC = () => {
+  return (
+    <div className={'set-count-block'}>
+      <div className={'set-count-screen'}>
+        <div className={'max-value'}>
+          <span>max value: </span>
+          <input type={'number'}/>
+        </div>
+        <div className={'start-value'}>
+          <span>start value: </span>
+          <input/>
+        </div>
+      </div>
+      <div className={'set-count-button'}>
+        <button>set</button>
+      </div>
+    </div>
+  )
+}
