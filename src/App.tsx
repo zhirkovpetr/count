b86dec458806548e1c860c690d47d071c3a26e17
Petr <zhirkovpetr95@gmail.com>
2023-09-20 12:16:17 +0300

    feat: add Count component and style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
-import './App.css';
 import {SetCount} from "./components/set-count/SetCount";
+import {Count} from "./components/count/Count";
+
+import './App.css';
 
 export const App: React.FC = () => {
   return (
     <div className="App">
       <SetCount/>
+      <Count/>
     </div>
   );
 }
